7fd7d8c79f7f7afd6dec4435e5c2ee7a8c70c93d
ayanshaikh18 <mahammadayan18@gmail.com>
2021-05-22 10:08:36 +0530

    frontend modified

--- frontend/script.js
+++ frontend/script.js
@@ -1,27 +1,35 @@
 $("#sendbtn").click(send_msg);
 
-const socket = io()
-const msg_input = $("#msg")
-const sender = $("#sender")
-const msg_div = $("#msgs")
+const socket = io();
+const msg_input = $("#msg");
+const sender = $("#sender");
+const msg_div = $("#msgs");
 
 function send_msg() {
-    let new_msg = {
-        "sender" : sender.val(),
-        "message" : msg_input.val()
-    }
-    msg_input.val("")
-    socket.emit("new-message",new_msg)
-    msg_div.append(`
-        <div class='me'>${new_msg.sender}<br />${new_msg.message}</div>
-    `)
-    msg_div.scrollTop(msg_div.height());
+  let new_msg = {
+    sender: sender.val(),
+    message: msg_input.val(),
+    time: new Date().getHours()+" : " + new Date().getMinutes(),
+  };
+  msg_input.val("");
+  socket.emit("new-message", new_msg);
+  msg_div.append(`
+        <div class='me'>
+            <div class='sender'>${new_msg.sender}</div>
+            <div class='msg'>${new_msg.message}</div>
+            <div class='time'>${new_msg.time}</div>
+        </div>
+    `);
+  msg_div.scrollTop(msg_div.height());
 }
 
-socket.on("got-new-msg",(msg)=>{
-    msg_div.append(`
-        <div class='you'>${msg.sender}<br />${msg.message}</div>
-    `)
-    msg_div.scrollTop(msg_div.height());
-    // $("#msgs").scrollTop($("#msgs").height());
-})
+socket.on("got-new-msg", (msg) => {
+  msg_div.append(`
+        <div class='you'>
+            <div class='sender'>${msg.sender}</div>
+            <div class='msg'>${msg.message}</div>
+            <div class='time'>${msg.time}</div>
+        </div>
+    `);
+  msg_div.scrollTop(msg_div.height());
+});
--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const express = require("express")
-const path = require("path")
-const http = require("http")
-const socket = require("socket.io")
-const PORT = 3000
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socket = require("socket.io");
+const PORT = 3000;
 
-const app = express()
-const server = app.listen(PORT,()=>{
-    console.log(`Server Started on Port ${PORT}`)
-})
+const app = express();
+const server = app.listen(PORT, () => {
+  console.log(`Server Started on Port ${PORT}`);
+});
 
-const io = socket(server)
+const io = socket(server);
 
-app.use(express.static(path.join(__dirname,"frontend")))
+app.use(express.static(path.join(__dirname, "frontend")));
 
-io.on("connection",(socket)=>{
-    console.log("New Client Connected")
+io.on("connection", (socket) => {
+  console.log("New Client Connected");
 
-    socket.on("new-message",(msg)=>{
-        console.log("New Msg :- "+msg)
-        socket.broadcast.emit("got-new-msg",msg)
-    })
-})
+  socket.on("new-message", (msg) => {
+    console.log("New Msg :- " + msg);
+    socket.broadcast.emit("got-new-msg", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client Disconnected");
+  });
+});
